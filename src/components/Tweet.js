3f1caa71745697f93f350d2b09eaa7e2355a04f4
Veda Vogirala <vedavogirala1991@users.noreply.github.com>
2020-12-22 02:34:34 +0530

    Udacity Tweet App - Formatting the tweets

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
+import {formatTweet,formatDate} from '../utils/helpers'
 
 class Tweet extends Component {
   render() {
+    const tweet = this.props.tweet
+    if(tweet === null){
+      return <p> This Tweet doesn't exists </p>
+    }
+
     return (
       <div className='tweet'>
-        <span>Posted tweet : <p>{this.props.tweet.text}</p> </span>
+        <span>{tweet.avatar}</span>
+        <span>{tweet.name}</span>
+        <span>{formatDate(tweet.timestamp)}</span>
+        <span>{tweet.replies}</span><span>{tweet.likes}</span>
       </div>
     )
   }
 }
 
-const mapStateToProps = ({tweets} , {id}) => {
+const mapStateToProps = ({authedUser, users, tweets} , {id}) => {
   const tweet = tweets[id]
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null
   return {
-    tweet : tweet
+    tweet : tweet ?
+      formatTweet(tweet,users[tweet.author],authedUser,parentTweet)
+        : null
   }
 }
 
